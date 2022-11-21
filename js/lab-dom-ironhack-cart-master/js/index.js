399f37ab6d33c35a3ac2a67b5b85dce0ec65aa38
Norman Leyva <nleyvagarcia@gmail.com>
2022-11-21 08:32:10 +0100

    actualizacion readme

--- js/lab-dom-ironhack-cart-master/js/index.js
+++ js/lab-dom-ironhack-cart-master/js/index.js
@@ -5,41 +5,33 @@ function updateSubtotal(product) {
   const price = product.querySelector('.price span').innerText;
   const quantity = product.querySelector('.quantity input').value;
   const subtotal = parseInt(price) * quantity;
-  const subtotalTd = product.querySelector(".subtotal span");
+  const subtotalTd = product.querySelector('.subtotal span');
   subtotalTd.innerText = subtotal;
 
   return subtotal;
 }
 
 function calculateAll() {
-
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   // const singleProduct = document.querySelector('.product');
   // updateSubtotal(singleProduct);
 
-
-
   // end of test
 
   // ITERATION 2
   //... your code goes here
-  const allProducts = document.querySelectorAll(".product");
+  const allProducts = document.querySelectorAll('.product');
   console.log(allProducts);
 
   let total = 0;
 
-  allProducts.forEach(item => total += updateSubtotal(item));
-
-
-
+  allProducts.forEach((item) => (total += updateSubtotal(item)));
 
   // ITERATION 3
   //... your code goes here
-  const totalPrice = document.querySelector("#total-value span");
+  const totalPrice = document.querySelector('#total-value span');
   totalPrice.innerText = total;
-
-
 }
 
 // ITERATION 4
@@ -48,9 +40,9 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
-  const removeButtons = document.querySelectorAll(".btn-remove");
-  
 
+  const removeButtons = document.querySelectorAll('.btn-remove');
+  removeButtons.addEventListener('click', removeProduct);
 }
 
 // ITERATION 5
@@ -64,6 +56,8 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
-  const removeButtons = document.querySelectorAll(".btn-remove");
-  removeButtons.forEach(button => button.addEventListener("click", removeProduct));
+  const removeButtons = document.querySelectorAll('.btn-remove');
+  removeButtons.forEach((button) =>
+    button.addEventListener('click', removeProduct)
+  );
 });
