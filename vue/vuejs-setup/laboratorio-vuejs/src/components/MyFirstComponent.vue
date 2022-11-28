8248f456f31fc773720ada3a74689bc24b7f12a3
Norman Leyva <nleyvagarcia@gmail.com>
2022-11-28 15:31:09 +0100

    up vue lab

---
+++ vue/vuejs-setup/laboratorio-vuejs/src/components/MyFirstComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Mi nombre es {{myName}}, tengo {{myAge}} años y vivo en {{myCity}}</h1>
+    <div>
+        <button @click="clickHello">Click to Hello</button>
+    </div>
+</template>
+
+<script setup>
+import { ref, reactive } from "vue";
+
+// iteracion 2
+const myName = ref("Norman L");
+const myAge = ref(38);
+const myCity = ref("Barcelona");
+
+// iteracion 3 bono
+const clickHello = () => console.log('Hola Mundo');
+
+// iteracion 4
+
+
+</script>
+
+<style></style>
